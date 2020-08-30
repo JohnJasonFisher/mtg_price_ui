13e27710f4f73d9aaf87a101916c99ffb468453e
John Fisher <jjfisher91@gmail.com>
2020-08-30 13:54:32 -0500

    Copy from fnm-ui

---
+++ src/components/app/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import UsersList from '../users-list/UsersList'
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Welcome to Friday Night Magic!
+      </header>
+      <UsersList></UsersList>
+    </div>
+  )
+}
---
+++ src/components/users-list/UsersList.tsx
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
+export default function UsersList() {
+
+	const [ usersData, setUsers ] = useState([])
+
+	useEffect(() => {
+		const fetchUsers = async () => {
+			const result = await axios(
+				{ baseURL: 'http://localhost:8000/users' }
+			)
+			setUsers(result.data)
+		}
+		fetchUsers()
+	}, [])
+
+	const usersList = usersData.map(user => <li key={user}>{user}</li>)
+
+	return (
+		<div className="UsersList">
+			<ul>
+				{ usersList }
+			</ul>
+		</div>
+	)
+}
