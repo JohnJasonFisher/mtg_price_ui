429af80ca1c94c643e2bfd4fe807acea96ca2e59
johnf <johnf@topsteptrader.com>
2020-08-30 13:45:38 -0500

    Initialize app

---
+++ src/components/app/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import UsersList from '../users-list/UsersList'
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Welcome to the MTG Price UI!
+      </header>
+      <UsersList></UsersList>
+    </div>
+  )
+}
